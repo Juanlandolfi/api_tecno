ad4a50c18030ac7751f19afd8e315ce6840ff640
JuanLandolfi <juanignaciolandolfi@gmail.com>
2023-11-12 17:56:33 -0300

    allow cors all

--- src/main.py
+++ src/main.py
@@ -8,14 +8,14 @@ from src.routes.app_router import router
 
 app = FastAPI()
 
-origins = [
-    "http://localhost",
-    "http://localhost:8080",
-]
+# origins = [
+#     "http://localhost",
+#     "http://localhost:4200",
+# ]
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=['*'],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
